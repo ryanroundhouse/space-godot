dbed16ab0a1fa612f47b2c68a58e397b31050cb0
Ryan Graham <rg@ryangraham.ca>
2023-12-16 21:31:30 -0500

    asteroids have a random scale now.

--- actors/obstacles/asteroid.gd
+++ actors/obstacles/asteroid.gd
@@ -1,8 +1,13 @@
 extends Node2D
 
-func initialize(position: Vector2, direction: Vector2, rotation_speed: float):
+func initialize(position: Vector2, direction: Vector2, rotation_speed: float, scale: Vector2):
 	var primaryBody = find_child("primaryBody")
 	primaryBody.position = position
 	primaryBody.direction = direction
 	primaryBody.rotation_speed = rotation_speed
 	primaryBody.isPrimary = true
+	
+	var mainSprite = find_child("MainSprite")
+	mainSprite.scale = scale;
+	var collision = find_child("CollisionShape2D")
+	collision.scale = scale;
