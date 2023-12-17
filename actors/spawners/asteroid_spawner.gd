57b2933c7aab566b2d0b10af5226cc81784a8fa8
Ryan Graham <rg@ryangraham.ca>
2023-12-16 20:16:06 -0500

    now there's 30 random asteroids

--- actors/spawners/asteroid_spawner.gd
+++ actors/spawners/asteroid_spawner.gd
@@ -6,17 +6,18 @@ var asteroid_scene = preload("res://actors/obstacles/asteroid.tscn")
 func _ready():
 	randomize()
 	
-	# Create a random direction (angle)
-	var directionX = (randf() * 2.0 - 1.0) * 300
-	var directionY = (randf() * 2.0 - 1.0) * 300
-	
-	var asteroid_position = Vector2(50, 50)
-	var direction = Vector2(directionX, directionY)
-	
-	var asteroid = asteroid_scene.instantiate() 
-	asteroid.initialize(asteroid_position, direction)
-	add_child(asteroid)
-	print("spawned asteroid at (" + str(asteroid.position.x) + "," + str(asteroid.position.y) + ") with direction (" + str(directionX) + "," + str(directionY) + ") and rotation: " + str(rotation))
+	for numberOfAsteroids in range(30):
+		# Create a random direction (angle)
+		var directionX = (randf() * 2.0 - 1.0) * 300
+		var directionY = (randf() * 2.0 - 1.0) * 300
+		
+		var asteroid_position = Vector2(randi_range(-2000,2000), randi_range(-2000,2000))
+		var direction = Vector2(directionX, directionY)
+		
+		var asteroid = asteroid_scene.instantiate() 
+		asteroid.initialize(asteroid_position, direction)
+		add_child(asteroid)
+		print("spawned asteroid at (" + str(asteroid.position.x) + "," + str(asteroid.position.y) + ") with direction (" + str(directionX) + "," + str(directionY) + ") and rotation: " + str(rotation))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
