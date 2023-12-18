fdfa082728f0ca35b7b2d62486c109f9d4bfde74
Ryan Graham <rg@ryangraham.ca>
2023-12-17 22:06:42 -0500

    added asteroid party mode (P key)

--- actors/spawners/asteroid_spawner.gd
+++ actors/spawners/asteroid_spawner.gd
@@ -5,23 +5,9 @@ var asteroid_scene = preload("res://actors/obstacles/asteroid.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
-	var zone = find_parent("Zone")
 	#
 	for numberOfAsteroids in range(80):
-		# Create a random direction (angle)
-		var directionX = (randf() * 2.0 - 1.0) * 250
-		var directionY = (randf() * 2.0 - 1.0) * 250
-		var angular_velocity = randf_range(-3.0, 3.0)
-		var scaleAmount = randi_range(1, 3)
-		#var scale = Vector2(scaleAmount, scaleAmount)
-		
-		var asteroid_position = Vector2(randi_range(-zone.ZONE_WIDTH,zone.ZONE_WIDTH), randi_range(-zone.ZONE_HEIGHT,zone.ZONE_HEIGHT))
-		var linear_velocity = Vector2(directionX, directionY)
-		
-		var asteroid = asteroid_scene.instantiate() 
-		asteroid.initialize(asteroid_position, linear_velocity, angular_velocity, scaleAmount)
-		add_child(asteroid)
-		print("spawned asteroid at (" + str(asteroid.position.x) + "," + str(asteroid.position.y) + ") with linear_velocity (" + str(directionX) + "," + str(directionY) + ") and angular_velocity: " + str(angular_velocity))
+		spawnRandomAsteroid()
 		
 	#var position = Vector2(0,350)
 	#var linear_velocity = Vector2(150,38)
@@ -32,7 +18,23 @@ func _ready():
 	#asteroid.initialize(position, linear_velocity, angular_velocity, scale)
 	#add_child(asteroid)
 
+func spawnRandomAsteroid():
+	var zone = find_parent("Zone")
+	var directionX = (randf() * 2.0 - 1.0) * 250
+	var directionY = (randf() * 2.0 - 1.0) * 250
+	var angular_velocity = randf_range(-3.0, 3.0)
+	var scaleAmount = randi_range(1, 3)
+	#var scale = Vector2(scaleAmount, scaleAmount)
+	
+	var asteroid_position = Vector2(randi_range(-zone.ZONE_WIDTH,zone.ZONE_WIDTH), randi_range(-zone.ZONE_HEIGHT,zone.ZONE_HEIGHT))
+	var linear_velocity = Vector2(directionX, directionY)
+	
+	var asteroid = asteroid_scene.instantiate() 
+	asteroid.initialize(asteroid_position, linear_velocity, angular_velocity, scaleAmount)
+	add_child(asteroid)
+	print("spawned asteroid at (" + str(asteroid.position.x) + "," + str(asteroid.position.y) + ") with linear_velocity (" + str(directionX) + "," + str(directionY) + ") and angular_velocity: " + str(angular_velocity))
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta):
-	pass
+
+func _process(delta):
+	if Input.is_key_pressed(KEY_P):
+		spawnRandomAsteroid()
